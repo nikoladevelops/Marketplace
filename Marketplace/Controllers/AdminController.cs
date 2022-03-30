074821ec20a870f82d0155646af2dfbd6f67fa1a
Nikola <nikoladevelops@gmail.com>
2022-03-30 18:15:34 +0300

    Working on Admin privileges.

---
+++ Marketplace/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Marketplace.Models;
+using Marketplace.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        public IActionResult AdminPanel()
+        {
+            if (!(User.IsInRole(Helper.AdminRole)))
+            {
+                return NotFound();
+            }
+
+
+            return View();
+        }
+    }
+}
